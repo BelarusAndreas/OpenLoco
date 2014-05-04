2c8d4148a33575d8494a912b212f28afcef8a616
Tim Moran <tim@t-moran.com>
2014-05-04 11:55:20 +0100

    Tidy up

--- src/main/java/openloco/LocoTool.java
+++ src/main/java/openloco/LocoTool.java
@@ -5,13 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
 
 public class LocoTool {
 
@@ -20,8 +13,7 @@ public class LocoTool {
     public static void main(String[] args) throws IOException {
         final String DATA_DIR = args[0];
         Assets assets = new Assets();
-        DatFileLoader loader = new DatFileLoader(assets, DATA_DIR);
-        loader.loadFiles();
+        new DatFileLoader(assets, DATA_DIR).loadFiles();
     }
 
 }
