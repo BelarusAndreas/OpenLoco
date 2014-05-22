a2a2c305b0729c5b66883b4eb743f22b464a6bef
Tim Moran <tim@t-moran.com>
2014-05-22 21:49:18 +0100

    Load demo class from a map

--- src/main/java/openloco/demo/DemoPicker.java
+++ src/main/java/openloco/demo/DemoPicker.java
@@ -5,18 +5,32 @@ import openloco.datfiles.DatFileLoader;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class DemoPicker {
+
+    private static final Map<String, Class<? extends BaseDemo>> demoClasses = mapifyClasses();
+
+    private static Map<String, Class<? extends BaseDemo>> mapifyClasses() {
+        return Arrays.asList(LoadSprite.class, TerrainDemo.class)
+                .stream()
+                .collect(Collectors.toMap(Class::getSimpleName, Function.identity()));
+    }
+
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         final String DATA_DIR = System.getProperty("openloco.dataDir");
         Assets assets = new Assets();
         new DatFileLoader(assets, DATA_DIR).loadFiles();
 
-        Class<? extends BaseDemo> klass = (Class<? extends BaseDemo>) Class.forName("openloco.demo.LoadSprite");
-
-        BaseDemo demo = klass.getConstructor(Assets.class).newInstance(assets);
-        demo.run();
+        if (args.length > 0) {
+            Class<? extends BaseDemo> klass = demoClasses.get(args[0]);
+            BaseDemo demo = klass.getConstructor(Assets.class).newInstance(assets);
+            demo.run();
+        }
     }
 
 }
