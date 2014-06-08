f698e1985bcb23e6e0b377c0f989849703f0105a
Tim Moran <tim@t-moran.com>
2014-06-08 15:06:33 +0100

    Calculate sleeper and rail indices

--- src/main/java/openloco/rail/TrackRenderer.java
+++ src/main/java/openloco/rail/TrackRenderer.java
@@ -69,7 +69,7 @@ public class TrackRenderer implements Renderer<TrackNetwork>{
                 { {0, 0}, {0,-1}, {-1,0}, {-1,-1} }
         };
 
-        drawTrackPiece(track, spriteInstances, node, 4, 4, deltas, 24, 40, 56);
+        drawTrackPiece(track, spriteInstances, node, 4, 4, deltas, 24);
     }
 
     private void drawMediumCurve(Track track, List<SpriteInstance> spriteInstances, TrackNode node) {
@@ -80,14 +80,14 @@ public class TrackRenderer implements Renderer<TrackNetwork>{
                 { {0, 0}, {-1,0}, {-1,-1}, {-2,-1}, {-2,-2} }
         };
 
-        drawTrackPiece(track, spriteInstances, node, 5, 4, deltas, 136, 156, 176);
+        drawTrackPiece(track, spriteInstances, node, 5, 4, deltas, 136);
     }
 
-    private void drawTrackPiece(Track track, List<SpriteInstance> spriteInstances, TrackNode node, int spritesPerTile, int maxRotation, int[][][] deltas, int ballastStartIndex, int sleeperStartIndex, int railStartIndex) {
+    private void drawTrackPiece(Track track, List<SpriteInstance> spriteInstances, TrackNode node, int spritesPerTile, int maxRotation, int[][][] deltas, int spriteStartIndex) {
         int rotation = node.getRotation() % maxRotation;
-        int ballastIndex = ballastStartIndex + spritesPerTile * rotation;
-        int sleeperIndex = sleeperStartIndex + spritesPerTile * rotation;
-        int railIndex = railStartIndex + spritesPerTile * rotation;
+        int ballastIndex = spriteStartIndex + spritesPerTile * rotation;
+        int sleeperIndex = spriteStartIndex + ((maxRotation + rotation) * spritesPerTile);
+        int railIndex = spriteStartIndex + (2 * maxRotation + rotation) * spritesPerTile;
 
         for (int i = 0; i < spritesPerTile; i++) {
             OpenGlSprite ballastSprite = OpenGlSprite.createFromRawSprite(track.getSprites().get(ballastIndex + i));
