a0e9d4a2b5466da8a465bb484637eda9a741765c
Tim Moran <tim@t-moran.com>
2014-06-21 11:14:53 +0100

    Tidy up

--- src/main/java/openloco/demo/TrackDemo.java
+++ src/main/java/openloco/demo/TrackDemo.java
@@ -40,18 +40,6 @@ public class TrackDemo extends BaseDemo {
         }
         network.addAll(trackLayer.getNodes());
 
-        /*
-        network.add(new TrackNode(18, 18, 0, Track.TrackPiece.SMALLCURVE, 0));
-        network.add(new TrackNode(20, 17, 0, Track.TrackPiece.SMALLCURVE, 1));
-        network.add(new TrackNode(21, 19, 0, Track.TrackPiece.SMALLCURVE, 2));
-        network.add(new TrackNode(19, 20, 0, Track.TrackPiece.SMALLCURVE, 3));
-
-        network.add(new TrackNode(18, 18, 0, Track.TrackPiece.MEDIUMCURVE, 0));
-        network.add(new TrackNode(21, 16, 0, Track.TrackPiece.MEDIUMCURVE, 1));
-        network.add(new TrackNode(23, 19, 0, Track.TrackPiece.MEDIUMCURVE, 2));
-        network.add(new TrackNode(20, 21, 0, Track.TrackPiece.MEDIUMCURVE, 3));
-        */
-
         trackLayer = new TrackLayer(18, 24, 0, Orientation.N);
         trackLayer.addSBend(CurveDirection.LEFT);
         trackLayer.addSBend(CurveDirection.RIGHT);
