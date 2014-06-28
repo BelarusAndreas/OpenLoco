1939f54440a2164f0136d8dbd8877609a1069a27
Tim Moran <tim@t-moran.com>
2014-06-28 17:42:49 +0100

    Render bridges for small curves

---
+++ src/main/java/openloco/rail/BridgeTileType.java
@@ -0,0 +1,45 @@
+package openloco.rail;
+
+public enum BridgeTileType {
+
+    FULL_WALL_N(i(63), i(1), none()),
+    FULL_WALL_W(i(39), i(1), none()),
+    FULL_WALL_E(i(40), i(36, 37, 38), i(18, 19)),
+    FULL_WALL_S(i(64), i(60, 61, 62), i(26, 27)),
+    HALF_NW(i(12), i(11), i(34, 35)),
+    HALF_NE(i(14), i(2), none()),
+    HALF_SE(i(13), i(3), none()),
+    HALF_SW(i(15), i(4), none()),
+    NONE(none(), none(), none());
+
+    private final int[] wallSprites;
+    private final int[] baseSprites;
+    private final int[] supportSprites;
+
+    private BridgeTileType(int[] wallSprites, int[] baseSprites, int[] supportSprites) {
+        this.wallSprites = wallSprites;
+        this.baseSprites = baseSprites;
+        this.supportSprites = supportSprites;
+    }
+
+    public int[] getWallSprites() {
+        return wallSprites;
+    }
+
+    public int[] getBaseSprites() {
+        return baseSprites;
+    }
+
+    public int[] getSupportSprites() {
+        return supportSprites;
+    }
+
+    private static int[] i(int... indices) {
+        return indices;
+    }
+
+    private static int[] none() {
+        return new int[0];
+    }
+
+}
--- src/main/java/openloco/rail/TrackRenderer.java
+++ src/main/java/openloco/rail/TrackRenderer.java
@@ -9,6 +9,8 @@ import openloco.graphics.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static openloco.rail.BridgeTileType.*;
+
 public class TrackRenderer implements Renderer<TrackNetwork>{
 
     private final Assets assets;
@@ -94,7 +96,15 @@ public class TrackRenderer implements Renderer<TrackNetwork>{
                 { {0, 0}, {0,-1}, {-1,0}, {-1,-1} }
         };
 
+        BridgeTileType[][] bridgeTileTypes = {
+            { FULL_WALL_W, HALF_NW, HALF_SE, FULL_WALL_N },
+            { FULL_WALL_N, HALF_NE, HALF_SW, FULL_WALL_E },
+            { FULL_WALL_E, HALF_SE, HALF_NW, FULL_WALL_S },
+            { FULL_WALL_S, HALF_SW, HALF_NE, FULL_WALL_W }
+        };
+
         drawTrackPiece(track, spriteInstances, node, 4, 4, deltas, 24);
+        drawBridgeTiles(spriteInstances, node, deltas, bridgeTileTypes);
     }
 
     private void drawMediumCurve(Track track, List<SpriteInstance> spriteInstances, TrackNode node) {
@@ -242,5 +252,51 @@ public class TrackRenderer implements Renderer<TrackNetwork>{
             spriteInstances.add(new SpriteInstance(sleeperSprite, screenX, screenY, SpriteLayer.SLEEPERS, cartX, cartY, Tile.HEIGHT_STEP * node.getZ()));
             spriteInstances.add(new SpriteInstance(railSprite, screenX, screenY, SpriteLayer.RAILS, cartX, cartY, Tile.HEIGHT_STEP * node.getZ()));
         }
+    }
+
+    private void drawBridgeTiles(List<SpriteInstance> sprites, TrackNode node, int[][][] allOffsets, BridgeTileType[][] allBridgeTileTypes) {
+        int[][] offsets = allOffsets[node.getRotation()];
+        BridgeTileType[] bridgeTileTypes = allBridgeTileTypes[node.getRotation()];
+
+        Bridge bridge = assets.getBridge(node.getBridgeType());
+
+        for (int i=0; i<offsets.length; i++) {
+            drawBridgeTile(sprites, bridge, node, offsets[i], bridgeTileTypes[i]);
+        }
+    }
+
+    private void drawBridgeTile(List<SpriteInstance> sprites, Bridge bridge, TrackNode trackNode, int[] offset, BridgeTileType bridgeTileType) {
+        int[] walls = bridgeTileType.getWallSprites();
+        int[] base = bridgeTileType.getBaseSprites();
+        int[] supports = bridgeTileType.getSupportSprites();
+
+        int x = (trackNode.getX()+offset[0])*Tile.WIDTH;
+        int y = (trackNode.getY()+offset[1])*Tile.WIDTH;
+        int z = trackNode.getZ()*Tile.HEIGHT_STEP;
+
+        for (int u=0; u<walls.length; u++) {
+            int wallSprite = walls[u];
+            outputBridgeSprite(sprites, bridge, wallSprite, x, y, z);
+        }
+
+        for (int v=0; v<base.length; v++) {
+            int baseSprite = base[v];
+            outputBridgeSprite(sprites, bridge, baseSprite, x, y, z-Tile.HEIGHT_STEP);
+        }
+
+        for (int h=trackNode.getZ()-2; h>=0; h--) {
+            for (int s=0; s<supports.length; s++) {
+                int supportSprite = supports[s];
+                outputBridgeSprite(sprites, bridge, supportSprite, x, y, h*Tile.HEIGHT_STEP);
+            }
+        }
+    }
+
+    private void outputBridgeSprite(List<SpriteInstance> sprites, Bridge bridge, int spriteIndex, int cartX, int cartY, int cartZ) {
+        OpenGlSprite sprite = OpenGlSprite.createFromRawSprite(bridge.getSprites().get(spriteIndex));
+        int screenX = Math.round(IsoUtil.isoX(cartX, cartY, cartZ));
+        int screenY = Math.round(IsoUtil.isoY(cartX, cartY, cartZ));
+        sprites.add(new SpriteInstance(sprite, screenX, screenY, SpriteLayer.BRIDGE, cartX, cartY, cartZ));
+
     }
 }
